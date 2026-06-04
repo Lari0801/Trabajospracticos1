f8dd5e14a171286b070f8ea7f690f9a16ba68255
alumno <alumno@Instituto.local>
2026-06-04 09:07:39 -0300

    04/06/2026

--- tp5/app.js
+++ tp5/app.js
@@ -1,8 +1,34 @@
+//variables
 let number1 = 25;
 let number2 = 23;
 let resultado
+let suma
+let Resta
+let multiplicación
+let división
+let Apellido = "Gómez"
+let Nombre= "Belen"
+let edad = 16;
+//sumar
+suma = number1 + number2;
 
-resultado = number1 + number2;
+alert(' el resultado de sumar '+ number1 +' y ' + number2 + ' es '+ suma);
 
-console.log(resultado);
-alert(resultado)
+
+//mul
+multiplicación = number1 * number2;
+
+console.log(' el resultado de multiplicar '+ number1 +' y ' + number2 + ' es '+ multiplicación);
+
+//división
+división = number1 / number2;
+
+console.log(' el resultado de dividir '+ number1 +' y ' + number2 + ' es '+ división);
+
+//Resta
+Resta = number1 - number2;
+
+alert(' el resultado de restar '+ number1 +' y ' + number2 + ' es '+ Resta);
+
+//nombre
+console.log (' la alumna '+  Nombre  +  Apellido + ' tiene '+ edad + 'años' )
