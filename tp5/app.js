33d7fb02b8d4a07e78ab0a970e3fe06d6608fd04
Lari0801 <52178023@ivt.edu.ar>
2026-06-08 22:35:27 -0300

    080626

--- tp5/app.js
+++ tp5/app.js
@@ -9,6 +9,14 @@ let división
 let Apellido = "Gómez"
 let Nombre= "Belen"
 let edad = 16;
+let precio1 = 1;
+let precio2 = 2;
+let precio3 = 3;
+let total = 6;
+let comidafav = "pasta"
+let colorfav = "violeta"
+let numerfav = "7"
+let pais= 'Argentina'
 //sumar
 suma = number1 + number2;
 
@@ -32,3 +40,16 @@ alert(' el resultado de restar '+ number1 +' y ' + number2 + ' es '+ Resta);
 
 //nombre
 console.log (' la alumna '+  Nombre  +  Apellido + ' tiene '+ edad + 'años' )
+
+//precios
+console.log ('el precio total a pagar es:'+ ' '+' $'+ (precio1 + precio2 + precio3))
+
+//Cosas favoritas
+console.log ('Mi comida favorita es'+' '+ comidafav+',' +'me gusta el color'+ ' '+ colorfav+' '+'y mi número favorito es el'+' '+ numerfav )
+
+//País
+console.log(pais)
+pais = "Brasil"
+console.log(pais);
+
+//Trabajo Hecho Por Lara Diaz Gatica
