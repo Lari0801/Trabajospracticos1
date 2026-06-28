89a60466e0905bf70747d9f84631bc2e15b48add
Lari0801 <52178023@ivt.edu.ar>
2026-06-28 15:28:28 -0300

    28/06/26
    TP 6

--- práctica/app.js
+++ práctica/app.js
@@ -4,6 +4,16 @@ let boton3 = document.querySelector ('#b3')
 let boton4 = document.querySelector ('#b4')
 let parrafo = document.querySelector('#p1')
 
-boton1.onclick = function (){
-    parrafo.style.backgroundColor = 'blue'
+boton1.onclick = () => {
+    parrafo.textContent = "Que haces que no fuiste a escuchar Run It? Dale amigo, es un temazo!";
+}
+
+boton2.onclick = function () {
+ parrafo.style.backgroundColor = 'rgb(88, 147, 214)'
+}
+boton3.onclick = function () {
+    parrafo.style.color = 'rgb(51, 64, 80)'
+}
+boton4.onclick = function () {
+    parrafo.style.fontFamily = 'impact'
 }
