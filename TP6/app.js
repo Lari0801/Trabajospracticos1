f87750009d6ae82cd8ad97b7b9f01cb9b5bed3a8
Lari0801 <52178023@ivt.edu.ar>
2026-06-29 17:38:01 -0300

    29/06/2026

--- TP6/app.js
+++ TP6/app.js
@@ -4,7 +4,7 @@ let boton3 = document.querySelector ('#b3')
 let boton4 = document.querySelector ('#b4')
 let parrafo = document.querySelector('#p1')
 
-boton1.onclick = () => {
+boton1.onclick = function () {
     parrafo.textContent = "Que haces que no fuiste a escuchar Run It? Dale amigo, es un temazo!";
 }
 
