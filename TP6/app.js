7e4c87feb5ca5a0c9452b4ccb461aa5d727de35b
Lari0801 <52178023@ivt.edu.ar>
2026-06-29 18:14:50 -0300

    29/06/26

--- TP6/app.js
+++ TP6/app.js
@@ -2,6 +2,8 @@ let boton1 = document.querySelector ('#b1')
 let boton2 = document.querySelector ('#b2')
 let boton3 = document.querySelector ('#b3')
 let boton4 = document.querySelector ('#b4')
+let boton5 = document.querySelector('#b5')
+let boton6 = document.querySelector("#b6")
 let parrafo = document.querySelector('#p1')
 
 boton1.onclick = function () {
@@ -16,4 +18,14 @@ boton3.onclick = function () {
 }
 boton4.onclick = function () {
     parrafo.style.fontFamily = 'impact'
+}
+boton5.onclick = function () {
+    parrafo.style.fontSize = '24px'
+}
+boton6.onclick = function () {
+    parrafo.textContent = "Se llama Run It y está en todas las plataformas digitales. Miren el M/V en la cuenta oficial."
+    parrafo.style.backgroundColor = ""
+    parrafo.style.color = ""
+    parrafo.style.fontFamily = ""
+    parrafo.style.fontSize = ""
 }
