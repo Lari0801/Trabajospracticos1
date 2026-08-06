e3aafeba79852a87c72df29cb51100b82278c18b
alumno <52178023@ivt.edu.ar>
2026-08-06 08:53:29 -0300

    060826

--- TP7/app.js
+++ TP7/app.js
@@ -12,12 +12,12 @@ boton1.onclick = function () {
     }
 }
 //actividad 2
-let nombreUsuario = "Nahuel";
+let nombreUsuario2 = "Nahuel";
 let parrafo2 = document.querySelector("#p2");
 let boton2 = document.querySelector("#b2");
 
 boton2.onclick = function () {
-    if (nombreUsuario == "Nahuel") {
+    if (nombreUsuario2 == "Nahuel") {
         parrafo2.textContent = "Bienvenido Nahuel, ¿cómo estás?";
     } else {
         parrafo2.textContent = "Bienvenido usuario";
@@ -25,26 +25,50 @@ boton2.onclick = function () {
 }
 
 //actividad 3
-let nombreUsuario2 = "Marcos";
+let nombreUsuario3 = "Marcos";
 let parrafo3 = document.querySelector("#p3")
 let boton3 = document.querySelector("#b3")
 
 boton3.onclick = function () {
-if (nombreUsuario2 == "Nahuel" || nombreUsuario2 == "Marcos") {
-    parrafo3.textContent = "Bienvenido " + nombreUsuario2 + ", ¿cómo estás?";
+if (nombreUsuario3 == "Nahuel" || nombreUsuario3 == "Marcos") {
+    parrafo3.textContent = "Bienvenido " + nombreUsuario3 + ", ¿cómo estás?";
 } else {
-    parrafo3.textContent = "Bienvenido " + nombreUsuario2;
+    parrafo3.textContent = "Bienvenido " + nombreUsuario3;
 }
 }
 
 //actividad 4
-let numero = 0;
+let numero4 = 0;
 let parrafo4 = document.querySelector("#p4");
+let boton4 = document.querySelector("#b4")
 
-if (numero > 0) {
+boton4.onclick = function (){
+if (numero4 > 0) {
     parrafo4.textContent = "El número es positivo";
-} else if (numero < 0) {
+} else if (numero4 < 0) {
     parrafo4.textContent = "El número es negativo";
 } else {
     parrafo4.textContent = "El número es cero";
 }
+}
+//actividad 5
+let numero5 = 28;
+let boton5 = document.querySelector("#b5")
+let parrafo5 = document.querySelector("#p5")
+
+boton5.onclick = function (){
+    if (numero5 >= 6 && numero5 <= 11)  {
+        parrafo5.textContent = "Eres un niño";
+    } else if (numero5 >= 12 && numero5 <= 18) {
+        parrafo5.textContent = "Eres adolescente";
+    }else if (numero5 >= 19 && numero5 <= 26)  {
+        parrafo5.textContent = " Eres Joven";
+    } else if (numero5 >= 27 && numero5 <= 59) {
+        parrafo5.textContent = "Eres adulto";
+    }else if(numero5 >=60){
+    parrafo5.textContent = "Eres viejo";
+    }else parrafo5.textContent = " Hay problemas";
+
+}
+
+//actividad 6
