8b8681ce8c85cf893d8b682831c7e29c1ae04884
Lari0801 <52178023@ivt.edu.ar>
2026-09-23 18:58:17 -0300

    230926

---
+++ TP extra 2/app.js
@@ -0,0 +1,5 @@
+//actividad 1
+let edad = document.querySelector("#edad1");
+let cantidad = document.querySelector("#cantidad1");
+let boton = document.querySelector("#calcular1");
+let resultado = document.querySelector("#resultado1");
